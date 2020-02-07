49f70afb1944694c73fd90eed3d310027d4b818d
Mona Safari <60340719+Mona-Safari@users.noreply.github.com>
2020-02-06 20:14:57 -0800

    Update Portfolio3.js

--- Portfolio3.js
+++ Portfolio3.js
@@ -1,12 +1,9 @@
 var quotes =['Take it easy — but take it. ~Woody Guthrie',
-                   '',
-                   '',
-                   '',
-                   '',
-                   '',
-                   '',
-                   '',
-                   ''];
+                   'When you do things from your soul, you feel a river moving in you, a Joy. ~Rumi',
+                   'Listen to silence, it has so much to say. ~Rumi',
+                   'Build your own dreams, or someone else will hire you to build theirs. ~Farraj Gray',
+                   'It is not the strongest of the species that survives, nor the most intelligent. It is the one that is most adaptable. ~Charles Darwin'
+             ];
 
 function newQuote(){
   var randomNumber= Math.floor(Math.random()*(quotes.length));
